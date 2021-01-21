2784fb04aaba46f01a980b4ac4ddc5967d075196
Omar El Tarzi <omartarzi@gmail.com>
2021-01-20 23:31:01 -0800

    Completed lesson 3.

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,10 +9,48 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const product = (_, args) => {
+  return Product.findById(args.id)
+    .lean() // converts to JSON but isn't necessary
+    .exec()
+}
+
+const products = () => {
+  return Product.find({}).exec()
+}
+
+const newProduct = (_, args, ctx) => {
+  return Product.create({ ...args.input, createdBy: ctx.user._id })
+}
+
+const updateProduct = (_, args) => {
+  return Product.findByIdAndUpdate(args.id, args.input, { new: true }) // return value of this query to be the product AFTER i update it, not before
+    .lean()
+    .exec()
+}
+
+const removeProduct = (_, args) => {
+  return Product.findByIdAndRemove(args.id)
+    .lean()
+    .exec()
+}
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    products,
+    product
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy(product) {
+      return User.findById(product.createdBy)
+        .lean()
+        .exec()
+    }
   }
 }
