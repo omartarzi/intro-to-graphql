480e1189f224a432cb410039825cbcebb9272588
Omar El Tarzi <omartarzi@gmail.com>
2021-01-20 22:54:51 -0800

    Completed lesson 2.

--- src/server.js
+++ src/server.js
@@ -11,16 +11,65 @@ const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
   const rootSchema = `
+    type Car {
+      owner: Owner!
+      name: String!
+      age: Int
+    }
+
+    type Owner {
+      name: String
+      cat: Cat!
+    }
+
+    type Query {
+      cat(name: String!): Cat!
+      owner(name: String!): Owner!
+    }
+
     schema {
       query: Query
-      mutation: Mutation
     }
   `
   const schemaTypes = await Promise.all(types.map(loadTypeSchema))
 
   const server = new ApolloServer({
     typeDefs: [rootSchema, ...schemaTypes],
-    resolvers: merge({}, product, coupon, user),
+    resolvers: {
+      Query: {
+        cat(_, args, ctx, info) {
+          return {
+            // eslint-disable-next-line spaced-comment
+            /*name: args.name, age: 3, owner: {} */
+          }
+        },
+        owner(_, args) {
+          return {
+            // eslint-disable-next-line spaced-comment
+            /*name: args.name, cat: {} */
+          }
+        }
+      },
+      Cat: {
+        name() {
+          return 'Daryl'
+        },
+        age() {
+          return 2
+        },
+        owner() {
+          return {}
+        }
+      },
+      Owner: {
+        name() {
+          return 'Scott'
+        },
+        cat() {
+          return {}
+        }
+      }
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }
